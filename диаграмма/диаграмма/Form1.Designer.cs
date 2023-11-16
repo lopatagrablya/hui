e8d604076ba5037d5f90f330dd52a00e1350493b
lopatagrablya <149692300+lopatagrablya@users.noreply.github.com>
2023-11-16 20:14:27 +0700

    Add files via upload

---
+++ диаграмма/диаграмма/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace диаграмма
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(52, 36);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            6,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(120, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(543, 156);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(49, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(146, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Количество видов товаров:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(49, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Данные о продажах";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(52, 291);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(543, 304);
+            this.chart1.TabIndex = 4;
+            this.chart1.Text = "chart1";
+            // 
+            // chart2
+            // 
+            chartArea2.Name = "ChartArea1";
+            this.chart2.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart2.Legends.Add(legend2);
+            this.chart2.Location = new System.Drawing.Point(613, 291);
+            this.chart2.Name = "chart2";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Pie;
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart2.Series.Add(series2);
+            this.chart2.Size = new System.Drawing.Size(543, 304);
+            this.chart2.TabIndex = 5;
+            this.chart2.Text = "chart2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1168, 639);
+            this.Controls.Add(this.chart2);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
+    }
+}
+
---
+++ диаграмма/диаграмма/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace диаграмма
+{
+    public partial class Form1 : Form
+    {
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+          
+            
+        }
+
+
+
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+
+
+
+        }
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            int rowsCount = (int)numericUpDown1.Value;
+
+            DataTable table = new DataTable();
+
+            dataGridView1.DataSource = table;
+
+            table.Columns.Add("№ п.п.", typeof(int));
+            table.Columns.Add("Товар", typeof(string));
+            table.Columns.Add("Цена", typeof(int));
+            table.Columns.Add("Количество", typeof(int));
+            table.Columns.Add("Стоимость", typeof(int));
+
+            table.Rows.Add("1", "Бананы", 10, 50, 500);
+            table.Rows.Add("2", "Киви", 20, 50, 1000);
+            table.Rows.Add("3", "Яблоки", 30, 50, 1500);
+            table.Rows.Add("4", "Апельсины", 15, 30, 450);
+            table.Rows.Add("5", "Мандарины", 28, 15, 420);
+            table.Rows.Add("6", "Виноград", 37, 22, 814);
+
+
+
+            if (rowsCount < table.Rows.Count)
+            {
+                
+                for (int i = table.Rows.Count - 1; i >= rowsCount; i--)
+                {
+                    table.Rows.RemoveAt(i);
+                }
+            }
+
+
+            chart1.Series.Clear();
+            chart1.Titles.Clear();
+            chart2.Series.Clear();
+            chart2.Titles.Clear();
+
+            var columnSeries = new Series("Стоимость");
+            columnSeries.ChartType = SeriesChartType.Column;
+
+            
+            var pieSeries = new Series("Стоимость");
+            pieSeries.ChartType = SeriesChartType.Pie;
+            //var series = new Series("Стоимость");
+            //series.ChartType = SeriesChartType.Column;//pie
+
+
+
+
+            int totalCost = 0;
+            foreach (DataRow row in table.Rows)
+            {
+                totalCost += Convert.ToInt32(row["Стоимость"]);
+            }
+
+            
+            for (int i = 0; i < table.Rows.Count; i++)
+            {
+
+                double percentage = Convert.ToDouble(table.Rows[i]["Стоимость"]) / totalCost * 100;
+
+                // Add points to the column series
+                columnSeries.Points.AddXY(table.Rows[i]["Товар"].ToString(), percentage);
+                columnSeries.Points[i].Label = "#PERCENT{P0}";
+                columnSeries.Points[i].LegendText = table.Rows[i]["Товар"].ToString() + " - " + table.Rows[i]["Стоимость"].ToString() + " руб.";
+
+                // Add points to the pie series
+                pieSeries.Points.AddXY(table.Rows[i]["Товар"].ToString(), percentage);
+                pieSeries.Points[i].Label = "#PERCENT{P0}";
+                pieSeries.Points[i].LegendText = table.Rows[i]["Товар"].ToString() + " - " + table.Rows[i]["Стоимость"].ToString() + " руб.";
+            }
+
+
+            chart1.Series.Add(columnSeries);
+            chart2.Series.Add(pieSeries);
+
+
+            chart1.Titles.Add("Стоимость товаров");
+            chart2.Titles.Add("Стоимость товаров");
+
+
+            chart1.Update();
+            chart2.Update();
+        }
+
+
+
+    }
+
+          
+    }
+
